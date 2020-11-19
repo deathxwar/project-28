031e1699095350d22c74205db1029d5786a812f5
ERGØ女DS <71591487+deathxwar@users.noreply.github.com>
2020-11-19 18:42:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var boy,tree;
+function preload(){
+	boy = loadImage("bog.png");
+    tree = loadImage("tree.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    boy.addImage(200,200)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
